761d13220fd0c5f7635f32ea10c0d55318a41fb6
usamar98 <connecttousama@gmail.com>
2025-07-27 05:42:45 +0500

    chsngd

--- client/src/components/TeamBundleWalletPage.js
+++ client/src/components/TeamBundleWalletPage.js
@@ -198,6 +198,15 @@ const TeamBundleWalletPage = () => {
     }).format(amount);
   };
 
+  const formatCurrency = (amount) => {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2
+    }).format(amount);
+  };
+
   // Cleanup on unmount
   useEffect(() => {
     return () => {
